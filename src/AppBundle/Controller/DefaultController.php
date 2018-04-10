e37a4e73dd41b899fd415884b9260d55e19eefe4
Rob <email@roberttreacy.com>
2018-04-10 20:28:42 +0100

    Added 2 column layout for frontend

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,17 +5,30 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityRepository;
+use AppBundle\Entity\Article;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/",
+     *  name="homepage",
+     *  requirements = {
+     *      "page": "\d+",
+     *      "per_page": "\d+"
+     *  },
+     *  defaults = {
+     *      "page": 1,
+     *      "per_page": 30
+     *  }
+     * )
      */
-    public function indexAction(Request $request)
+    public function indexAction(Request $request, int $page, int $per_page, EntityRepository $articleRepository)
     {
-        // replace this example code with whatever you need
+        $articles = $articleRepository->findBy([], null, $per_page, $page - 1);
+        
         return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+            'articles' => $articles,
         ]);
     }
 }
