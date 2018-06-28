7969ef9788646806fa50a1b65abdbbfbdcb553cd
Rob <email@roberttreacy.com>
2018-06-28 22:31:15 +0100

    Fixes #1 Added basic caching.

--- src/AppBundle/Controller/ArticleController.php
+++ src/AppBundle/Controller/ArticleController.php
@@ -5,17 +5,29 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use AppBundle\Entity\Article;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class ArticleController extends Controller
 {
     /**
      * @Route("/article/{path}", name="view_article")
      */
-    public function getAction(Article $article)
+    public function getAction(Request $request, Article $article)
     {
+        $response = new Response;
+
+        $response->setLastModified($article->getModifiedAt());
+        $response->setSharedMaxAge(3600);
+        $response->setpublic();
+
+        if ($response->isNotModified($request)) {
+            return $response;
+        }
+
         return $this->render('article/get.html.twig', array(
             'article' => $article,
-        ));
+        ), $response);
     }
 
 }
--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Doctrine\ORM\EntityRepository;
 use AppBundle\Entity\Article;
 
@@ -18,9 +19,25 @@ class DefaultController extends Controller
         $articles = $articleRepository->findBy([
             'status' => Article::STATUS_PUBLISHED,
         ], null, $request->query->getInt('per_page', 10), $request->query->getInt('page', 1) - 1);
-        
-        return $this->render('default/index.html.twig', [
+
+        $lastModified = null;
+        foreach($articles as $article) {
+            $lastModified = $lastModified ? max($lastModified, $article->getModifiedAt()) : $article->getModifiedAt();
+        }
+
+        $response = new Response;
+        $response->setLastModified($lastModified);
+        $response->setSharedMaxAge(3600);
+        $response->setPublic();
+
+        if ($response->isNotModified($request)) {
+            return $response;
+        }
+
+        $response = $this->render('default/index.html.twig', [
             'articles' => $articles,
-        ]);
+        ], $response);
+
+        return $response;
     }
 }
